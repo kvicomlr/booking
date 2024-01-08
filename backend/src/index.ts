f81965ba82d78f97bde4b49a5d9b25eccc1c8d6d
kvicomlr <kvi.com.lr@gmail.com>
2024-01-08 07:44:36 +0400

    Initial Commit of Source Codes

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+const app = express();
+
+const PORT = process.env.Port;
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//dBase
+mongoose.connect(process.env.mongoDB as string);
+
+app.get("/api/booking", async (req: Request, res: Response) => {
+  res.json({
+    meseage: "KVI Booking App",
+  });
+});
+
+//Server
+app.listen(PORT, () => {
+  console.log(`1. Server is running on port: ${PORT}`);
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout></Layout>} />
+        <Route path="/search" element={<>Search Page</>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
